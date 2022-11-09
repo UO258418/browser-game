6d309c20c855f54d0bea6fd46be7caeca3852b4b
UO258418 <UO258418@uniovi.es>
2022-11-09 05:04:10 +0100

    Removed enemy trial

--- src/enemy/enemytrial.js
+++
@@ -1,8 +0,0 @@
-class EnemyTrial extends Enemy {
-
-    constructor(x, y, speed, target) {
-        super(x, y, speed, target, 100, 1);
-        this.animation = new Animation(sprites.player, 4, 3, this, 8);
-    }
-
-}
